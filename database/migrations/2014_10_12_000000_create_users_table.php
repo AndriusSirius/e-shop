1d90ddca52fd0cc51f559b3397ebe416d048bc59
ZaweRD <78658299+ZaweRD@users.noreply.github.com>
2021-02-23 21:37:16 +0200

    DB users updated?

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('adress');
+            $table->string('city');
+            $table->string('country');
+            $table->string('postcode');
+            $table->string('telephone');
             $table->rememberToken();
             $table->timestamps();
         });
