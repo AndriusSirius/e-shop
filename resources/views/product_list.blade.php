a568c815e86cdf42242a4dd61297165e512a142b
codingfullstack <justinas.sadauskiu@gmail.com>
2021-03-03 18:48:54 +0200

    pakeitimai

--- resources/views/product_list.blade.php
+++ resources/views/product_list.blade.php
@@ -41,7 +41,7 @@
                <p class="text-center font-bold">10%</p>
             </div>
             <img class="object-contain w-full  h-82" src="{{ asset('images/kitchen.jpg') }}" alt="Trulli">
-            <figcaption class="font-semibold small-text mx-2 my-2 text-left md:text-center">Lorem ipsum dolor sit amet dolor sit amet dolor sit amet..</figcaption>
+            <figcaption class="font-semibold small-text mx-2 my-2 text-left md:text-center">Lore ipsum dolor sit amet dolor sit amet dolor sit amet..</figcaption>
          </figure>
          <button class="bg-white w-full p-2 hover:bg-blue-500 hover:text-white text-blue-500 border border-2 border-blue-700 transition duration-300 ease-in-out rounded-full text-lg focus:outline-none  focus:shadow-outline font-semibold "> <svg class="h-6  inline mx-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
