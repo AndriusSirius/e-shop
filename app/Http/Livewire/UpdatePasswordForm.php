5b48f7bce96f66c3bc9402f921fafd6c495cff18
ZaweRD <78658299+ZaweRD@users.noreply.github.com>
2021-03-16 15:03:18 +0200

    Admin profile on livwire

---
+++ app/Http/Livewire/UpdatePasswordForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UpdatePasswordForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.update-password-form');
+    }
+}
